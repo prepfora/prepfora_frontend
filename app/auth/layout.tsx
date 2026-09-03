a2b7b630026d295916682f19f990cee5dd7c00ab
egileekada <egileoniso.ekada@gmail.com>
2026-09-03 03:04:02 +0100

    feat: add auth layout component with split-screen image and suspense wrapper

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,19 +1,22 @@
 import { CustomImage } from "@/components/ui";
+import { Suspense } from "react";
 
 
-export default function Layout ({
+export default function Layout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return(
-        <div className=" w-full h-screen overflow-hidden flex bg-primary-50 " >
-            <div className=" w-full h-screen " >
-                <CustomImage src={"/images/auth.png"} alt="auth" layout="width" />
+    return (
+        <Suspense>
+            <div className=" w-full h-screen overflow-hidden flex bg-primary-50 " >
+                <div className=" w-full h-screen " >
+                    <CustomImage src={"/images/auth.png"} alt="auth" layout="width" />
+                </div>
+                <div className=" w-full h-screen overflow-y-auto py-10 px-6 flex justify-center" >
+                    {children}
+                </div>
             </div>
-            <div className=" w-full h-screen overflow-y-auto py-10 px-6 flex justify-center" >
-                {children}
-            </div>
-        </div>
+        </Suspense>
     )
 }
